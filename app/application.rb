2555ca93368c963886002ca484118b33f3cc061a
Garrett Thompson <gtcruz.gt@gmail.com>
2019-12-27 02:57:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
 
     if Time.now.hour.between?(0, 11)
       resp.write "Good Morning!"
-    elseif Time.now.hour.between?(12, 23)
+    elsif Time.now.hour.between?(12, 23)
       resp.write "Good Afternoon!"
     end
 
